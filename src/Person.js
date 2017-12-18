d3f4fa543861fd1a1882637679ade6b04483b799
Bophelo <mpilo@vivaldi.net>
2017-12-18 21:57:56 +0200

    Transformed and tested output file

--- src/Person.js
+++ src/Person.js
@@ -2,4 +2,10 @@ class Person {
     constructor(name) {
         this.name = name;
     }
+
+    greet() {
+        return this.name + ' says hello.'; //can be cleaned up a bit more using template strings (review)
+    }
 }
+
+console.log(new Person('Mpilo').greet());
